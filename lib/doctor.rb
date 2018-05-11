fdda86f0a01dde54641e08d365db896b07fc2972
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-05-11 00:39:54 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,9 +1,10 @@
 class Doctor
-  attr_accessor :name, :appointments
+  attr_accessor :name, :appointments, :patients
 
   def initialize(name)
     @name = name
     @appointments = []
+    @patients = [] 
   end
 
   def add_appointment(appointment)
