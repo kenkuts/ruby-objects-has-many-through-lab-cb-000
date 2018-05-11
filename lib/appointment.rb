da1160de1f95b74804d5385a950bacafc5ecc542
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-05-11 01:16:32 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -10,6 +10,10 @@ class Appointment
 end
 
 =begin
-  
-  
+  initialize - This method declares our date and doctor and immediately
+      calls our doctor classes add_appointment method
+
+  The patient will be added in after we have declared it. We can use the
+    patient setter mehtod to create it within the class without explicitly
+    creating a patient instance variable. 
 =end
