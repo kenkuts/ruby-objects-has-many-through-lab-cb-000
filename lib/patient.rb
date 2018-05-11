47716a48af64a28d010cd410284d9d61db2294c1
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-05-11 00:42:58 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -3,10 +3,16 @@ class Patient
 
   def initialize(name)
     @name = name
-    @doctor = []
-    @appointment = []
+    @doctors = []
+    @appointments = []
   end
 
+  def add_appointment(appointment)
+    @appointments << appointment
+    @doctors << appointment.doctor 
+  end
+
+  def 
 
 
 end
